8cbc8d1155a4c949131a554fcbba4842a6aa874b
Dmitry <solovushkov.d@yandex.ru>
2022-11-07 16:25:25 +0400

    lesson_4

---
+++ lessons/lesson_4/stack.java
@@ -0,0 +1,15 @@
+package lessons.lesson_4;
+import java.util.*;
+
+public class stack {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(12);
+        stack.push(123);
+        System.out.println(stack.pop()); // 123
+        System.out.println(stack.pop()); // 12
+        System.out.println(stack.pop()); // 1
+    }
+ 
+}
